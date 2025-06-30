442a348e082ce4db1362e333a6352e44aa2d6049
asim <asim.mahmood00786@gmail.com>
2025-06-30 21:41:01 +0530

    fix:fixed build issues

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
 import menuRoutes from './routes/menu.routes';
 import orderRoutes from './routes/order.routes';
 
 
-
-dotenv.config();
-
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
